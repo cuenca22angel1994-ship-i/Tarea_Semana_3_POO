2600bd24328baadab581f49947ed346e2de064d8
Raf.CT <cuenca22angel1994@gmail.com>
2026-06-13 21:10:34 -0500

    Update main.py

--- Programacion_poo/main.py
+++ Programacion_poo/main.py
@@ -3,7 +3,8 @@ from mascota import Mascota
 def main():
     
     mascota1 = Mascota("Rex", "Perro", 3)
-    mascota2 = Mascota("Coco", "Gato", 1)
+    mascota2 = Mascota("Coco", "Gato", 2)
+    mascota3 = Mascota("Pepin", "Loro", 2)
 
     print("=== DATOS DE LA PRIMERA MASCOTA ===")
     mascota1.mostrar_informacion()
@@ -11,7 +12,11 @@ def main():
 
     print("=== DATOS DE LA SEGUNDA MASCOTA ===")
     mascota2.mostrar_informacion()
-    print(f"Sonido: {mascota2.hacer_sonido()}")
+    print(f"Sonido: {mascota2.hacer_sonido()}\n")
+
+    print("=== DATOS DE LA TERCERA MASCOTA ===")
+    mascota3.mostrar_informacion()
+    print(f"Sonido: {mascota3.hacer_sonido()}")
 
 if __name__ == "__main__":
     main()
